8cf3344a678ebfd736d5f2796e2f861c5e5845a2
hamidrezapoorjavad <hamidpoorjavad_master@yahoo.com>
2020-10-25 23:59:10 +0330

    tagirat

--- app/src/main/java/com/example/ha_01/hello 2.kt
+++ app/src/main/java/com/example/ha_01/hello 2.kt
@@ -14,6 +14,51 @@ fun main() {
 //    val my = Array(30,{i ->i*3  })
 ////    print(my[9])
 
-    val name = Array(10, { i -> "ali$i" })
-    print(name[5])
+//    val name = Array(10, { i -> "ali$i" })
+//    print(name[5])
+//    println("enter your number")
+//    val a = readLine()!!.toInt()
+//    if (a>70 )
+//        print("pass")
+//
+//    else
+//        print("fail")
+//    var name: String
+//    println("enter 1 or 2")
+//    val number = readLine()!!.toInt()
+//    name = if (number == 1) {
+//        "ali"
+//    } else {
+//        "hamid"
+//    }
+//    print(name)
+//    println("enter your number")
+//    val number = readLine()!!.toInt()
+//    if (number in 80..100) {
+//        print("top student")
+//    } else if (number in 50..80) {
+//        print("pass")
+//    } else if (number in 30..50) {
+//        print("condition pass")
+//    } else if (number in 0..30) {
+//        print("fail")
+//    } else {
+//        print("wrong no")
+//    }
+    var name = readLine()!!.toInt()
+    when(name) {
+        1 -> {print("one")
+            print("two")}
+        2 -> print("two")
+        3 -> print("three")
+        4 -> print("four")
+        5 -> print("five")
+        6 -> print("six")
+        7 -> print("seven")
+        8 -> print("eight")
+        9 -> print("nine")
+        in 10..30-> print("ten")
+        else -> print("ff")
+    }
+
 }
